e1a3fe7b644505a8bf6c98abb411ecf21a3ed31b
Dalton Pearson <daltonpearson1997@gmail.com>
2018-07-06 14:51:07 -0400

    Working properly

--- background.js
+++ background.js
@@ -3,21 +3,18 @@
 // Close tabs with matching titles when the extension icon gets clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
 
-    console.log("background");
-
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
         var activeTab = tabs[0];
+
         var tabTitle = activeTab.title;
 
         console.log(activeTab + " : " + tabTitle);
 
         chrome.tabs.query({ title: tabTitle, currentWindow: true }, function (tabs) {
-            console.log(tabs);
 
             var tabIds = tabs.map(t => t.id);
 
-            console.log(tabIds);
             chrome.tabs.remove(tabIds);
         });
     });
@@ -25,16 +22,24 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
 // close all tabs with the title "Swagger UI" when they are opened
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
-    console.log(tabId + " " + info);
+
     if (info.status === 'complete') {
         
         chrome.tabs.query({ title: "Swagger UI" }, function (tabs) {
-           
-            console.log(tabs);
-           
-            var tabIds = tabs.map(t => t.id);
+            
+            if (tabs.length >= 1){
+                
+                var tabIds = tabs.map(t => t.id);
 
-            chrome.tabs.remove(tabIds);
+                chrome.tabs.remove(tabIds);
+
+                chrome.tabs.query({ title: "NCRI" }, function (tabs) {
+            
+                    if (tabs.length >= 1)
+                        chrome.tabs.update(tabs[0].id, {"active": true});
+                    
+                });
+            }
         });
     }
 });
